379a54ecc480fc186190803bef734c8b1d66ebdd
Hermelab1 <hermelabirhanu06@gmail.com>
2023-04-23 13:32:41 -0700

    Bubble Sort

--- swaping.c
+++ swaping.c
@@ -1,7 +1,14 @@
 #include "sort.h"
+
+/**
+ * swap - to swapping the array element
+ *
+ * @xp: index number
+ * @yp: index number
+ */
 void swap(int *xp, int *yp)
 {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
 }
