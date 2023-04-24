20e4aa933976a339a262e5c8d5469a5834ed6190
Hermelab1 <hermelabirhanu06@gmail.com>
2023-04-24 10:23:55 -0700

    Shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -11,18 +11,16 @@ void shell_sort(int *array, size_t size)
 	size_t gap = 1, i, j;
 	int temp;
 
-	while (gap < size / 3)
-		gap = gap * 3 + 1;
-
-	while (gap > 0)
+	for (gap = size/2; gap > 0; gap /= 2)
 	{
-		for (i = gap; i < size; i++)
+		for (i = gap; i < size; i += 1)
 		{
 			temp = array[i];
-			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			for(j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			{
 				array[j] = array[j - gap];
+			}
 			array[j] = temp;
 		}
-		gap = (gap - 1) / 3;
 	}
 }
